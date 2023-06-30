167058f8229d0ad54b4820f85a07c71080ec36b9
Dmitriy Sharandikov <custadialship@gmail.com>
2023-06-30 12:30:31 +0300

    master-3

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -10,6 +10,7 @@ public class DemoApplication {
         SpringApplication.run(DemoApplication.class, args);
         System.out.println("master-1");
         System.out.println("master-2");
+        System.out.println("master-3");
     }
 
 }
