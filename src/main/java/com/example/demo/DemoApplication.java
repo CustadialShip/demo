0612a2f67241beb7adb16544638a59fef01521f6
Dmitriy Sharandikov <custadialship@gmail.com>
2023-06-30 12:33:12 +0300

    mmm

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -12,6 +12,7 @@ public class DemoApplication {
         System.out.println("master-2");
         System.out.println("master-3");
         System.out.println("master-3");
+        System.out.println("master-3");
     }
 
 }
